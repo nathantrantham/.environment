3641d67c7005b0d99c7716e2706532151961898c
nathan trantham <nathan.trantham@adcolony.com>
2016-09-27 14:36:38 -0700

    Updating initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -7,14 +7,14 @@ mkdir -p ~/devel/data
 cd ~/devel/tools
 
 sudo apt-get update 
-sudo apt-get install -y default-jdk maven htop jq parallel tmux python2.7 awscli git zsh
+sudo apt-get install -y default-jdk maven htop jq parallel tmux python2.7 awscli git zsh scala vim-gnome
 
 #Download and install Scala
-curl -O http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
-tar -zxvf scala*
-rm *.tgz
+#curl -O http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
+#tar -zxvf scala*
+#rm *.tgz
  
 #Download and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
